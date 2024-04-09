5b3dc7499624bdf76c76e3ab8642721d50e2d63f
nicorassino <nicorassino@hotmail.com>
2024-04-09 15:23:55 -0300

    autogestionalumnos

--- inicio/loginResult.php
+++ inicio/loginResult.php
@@ -18,16 +18,33 @@ if ($result->num_rows > 0) {
   $_SESSION['login_message'] = "Inicio de sesión exitoso.";
 } else {
 // Consulta SQL para buscar el ALUMNO en la tabla de persona y password_alumnos
-$sql2 = "SELECT * FROM persona INNER JOIN alumnosterciario ON
-persona.idPersona=alumnosterciario.idPersona INNER JOIN passwords_alumnos ON
-alumnosterciario.idAlumno=passwords_alumnos.idAlumno WHERE persona.dni='$username' AND passwords_alumnos.password='$password'";
+$sql2 = "SELECT p.nombre,p.apellido,p.dni,a.idAlumno,p.idPersona FROM persona p INNER JOIN alumnosterciario a ON
+p.idPersona=a.idPersona INNER JOIN passwords_alumnos ON
+a.idAlumno=passwords_alumnos.idAlumno WHERE p.dni='$username' AND passwords_alumnos.password='$password'";
 $result2 = $conn->query($sql2);
 
-// Verificar si se encontró un registro en la base de datos
+
+
+// Verificar si se encontró un registro en la base de datos osea ingreso correcto
 if ($result2->num_rows > 0) {
-  // Usuario y contraseña válidos
   session_start();
-  $_SESSION['login_message'] = "Inicio de sesión exitoso.";
+
+  while($row = $result2->fetch_assoc()) {
+    $_SESSION['alu_nombre'] = $row["nombre"];
+    $_SESSION['alu_apellido'] = $row["apellido"];
+    $_SESSION['alu_dni'] = $row["dni"];
+    $_SESSION['alu_idAlumno'] = $row["idAlumno"];
+    $_SESSION['alu_idPersona'] = $row["idPersona"];
+
+}
+
+
+  $conn->close();
+
+  // Usuario y contraseña válidos
+
+   header("Location:../menu/alumnos.php");
+   exit();
 } else {
   // Usuario o contraseña incorrectos
   session_start();
